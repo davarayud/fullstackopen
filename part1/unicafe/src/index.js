54aaffc1cc8405cb0a3a02c13cbccad4f800d68b
unknown <davarayud@gmail.com>
2023-10-09 23:52:50 -0300

    add exercise 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-
+const StatisticLine = ({text, value, symbol}) => {
   return(
     <>
-      <h1>statistics</h1>
-      <p>
-        good {props.good} <br />
-        neutral {props.neutral} <br />
-        bad {props.bad} <br />
-        all {props.all} <br />
-        average {(props.good-props.bad)/props.all} <br />
-        positive {props.good*100/props.all}
-      </p>
+      {text} {value} {symbol}<br/>
     </>
   )
+}
+
+const Statistics = ({good, neutral, bad, all}) => {
+
+  if(all == 0){
+    return(
+        <p>No feedback given</p>
+    )
+  }
+  return(
+      <p>
+        <StatisticLine text={"good"} value={good} />
+        <StatisticLine text={"neutral"} value={neutral} />
+        <StatisticLine text={"bad"} value={bad} />
+        <StatisticLine text={"all"} value={all} />
+        <StatisticLine text={"average"} value={(good-bad)/all} />
+        <StatisticLine text={"neutral"} value={good*100/all} symbol={'%'} />
+      </p>
+  )
 } 
+
+const Button = ({handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -29,16 +47,10 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-
+      <Button handleClick={() => setGood(good + 1)} text={'good'} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'} />
+      <Button handleClick={() => setBad(bad + 1)} text={'bad'} />
+      <h1>statistics</h1>
       <Statistics good = {good} neutral = {neutral} bad = {bad} all={all}/>
     </div>
   )
