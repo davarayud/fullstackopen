6d70f86ccd718bbb53d5a48dcd7e45e481de243b
unknown <davarayud@gmail.com>
2023-10-16 11:44:34 -0300

    add exercise 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 
 const StatisticLine = ({text, value, symbol}) => {
   return(
-    <>
-      {text} {value} {symbol}<br/>
-    </>
+    <tr>
+      <td>{text} </td>
+      <td>{value} {symbol}</td>
+    </tr>
   )
 }
 
 const Statistics = ({good, neutral, bad, all}) => {
 
-  if(all == 0){
+  if(all === 0){
     return(
         <p>No feedback given</p>
     )
   }
   return(
-      <p>
+    <table>
+      <tbody>
         <StatisticLine text={"good"} value={good} />
         <StatisticLine text={"neutral"} value={neutral} />
         <StatisticLine text={"bad"} value={bad} />
         <StatisticLine text={"all"} value={all} />
         <StatisticLine text={"average"} value={(good-bad)/all} />
         <StatisticLine text={"neutral"} value={good*100/all} symbol={'%'} />
-      </p>
+      </tbody>
+    </table>
   )
 } 
 
@@ -56,6 +59,6 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root')
+const root = ReactDOMClient.createRoot(rootElement)
+root.render(<App />, )
